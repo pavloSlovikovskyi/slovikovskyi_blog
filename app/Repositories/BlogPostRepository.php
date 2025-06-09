e33ffa45a2ee767227695bb967b26050f00efc66
Pavlo Slovikovskyi <slovikovskuy06@gmail.com>
2025-06-09 23:11:52 +0300

    Лабораторна 7. Вивід статей блога

---
+++ app/Repositories/BlogPostRepository.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\BlogPost as Model;
+use Illuminate\Database\Eloquent\Collection;
+
+/**
+ * Class BlogPostRepository.
+ */
+class BlogPostRepository extends CoreRepository
+{
+    protected function getModelClass()
+    {
+
+        return Model::class;//абстрагування моделі BlogCategory, для легшого створення іншого репозиторія
+    }
+
+    /**
+     * Отримати список статей
+     * * @param int|null $perPage
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function getAllWithPaginate($perPage = null)
+    {
+        $columns = [
+            'id',
+            'title',
+            'slug',
+            'is_published',
+            'published_at',
+            'user_id',
+            'category_id',
+        ];
+
+        $result = $this->startConditions()
+            ->select($columns)
+            ->orderBy('id', 'DESC')
+            ->paginate($perPage ?? 25);
+
+        return $result;
+    }
+
+    /**
+     * Отримати модель для редагування в адмінці
+     * @param int $id
+     * @return Model
+     */
+    public function getEdit($id)
+    {
+        return $this->startConditions()->find($id);
+    }
+}
