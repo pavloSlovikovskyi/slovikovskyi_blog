690d7cb034c8ec2802064a875f125b065674e906
Pavlo Slovikovskyi <slovikovskuy06@gmail.com>
2025-06-17 21:17:59 +0300

    Лабораторна 13. Ланцюги завдань (Chain)

--- app/Http/Controllers/DiggingDeeperController.php
+++ app/Http/Controllers/DiggingDeeperController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\BlogPost;
 use Carbon\Carbon;
 use Illuminate\Support\Collection; // Додано для підказки типу Collection
+use App\Jobs\ProcessVideoJob;
+use App\Jobs\GenerateCatalog\GenerateCatalogMainJob;
 
 class DiggingDeeperController extends Controller
 {
@@ -140,5 +142,23 @@ class DiggingDeeperController extends Controller
 
         // Просто повертаємо порожній рядок, оскільки основна логіка демонстрації через dd()
         return '';
+    }
+    public function processVideo()
+    {
+        ProcessVideoJob::dispatch();
+        //  Відкладення виконання завдання від моменту потрапляння в чергу.
+        //  Не впливає на паузу між спробами виконання завдання.
+        //->delay(10)
+        //->onQueue('name_of_queue')
+    }
+
+    /**
+     * @link http://localhost:8000/digging_deeper/prepare-catalog
+     *
+     * php artisan queue:listen --queue=generate-catalog --tries=3 --delay=10
+     */
+    public function prepareCatalog()
+    {
+        GenerateCatalogMainJob::dispatch();
     }
 }
